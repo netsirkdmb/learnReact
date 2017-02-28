fb5895bbba3252fcda2f78e7abc4b3e08edd37b5
netsirkdmb <kristendhuse@gmail.com>
2017-02-27 20:54:39 -0800

    adding battle components and route

--- app/index.js
+++ app/index.js
@@ -6,56 +6,11 @@
     Testable
 */
 
-/*var USER_DATA = {
-    name: "Kristen Dhuse",
-    username: "netsirkdmb",
-    image: "https://avatars3.githubusercontent.com/u/8966199?v=3"
-}*/
-
 var React = require("react");
 var ReactDOM = require("react-dom");
 var routes = require("./config/routes");
 
-/*var ProfilePic = React.createClass({
-    render: function () {
-        return <img src={this.props.imageUrl} style = {{height: 100, width: 100}} />
-    }
-});
-
-var ProfileLink = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <a href={"https://www.github.com/" + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function () {
-        return (
-            <div>{this.props.name}</div>
-        )
-    }
-});
-
-var Avatar = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image}/>
-                <ProfileName name={this.props.user.name}/>
-                <ProfileLink username={this.props.user.username}/>
-            </div>
-        )
-    }
-});*/
-
 ReactDOM.render(
-    // <Avatar user={USER_DATA}/>,
     routes,
     document.getElementById("app")
 );
