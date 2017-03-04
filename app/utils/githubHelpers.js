e854b4258a6453d0bde76da6d65ec9a9991dd7b8
netsirkdmb <kristendhuse@gmail.com>
2017-03-03 16:48:13 -0800

    create results route

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -5,7 +5,46 @@ var sec = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sec;
 
 function getUserInfo (username) {
+    // returns a promise
     return axios.get("https://api.github.com/users/" + username);
+    // if getting rate limited fill in variables above and use this instead
+    // return axios.get("https://api.github.com/users/" + username + param);
+}
+
+function getRepos (username) {
+    // fetch usernames repos
+    return axios.get("https://api.github.com/users/" + username + "/repos?per_page=100");
+    // if getting rate limited fill in variables above and use this instead
+    // return axios.get("https://api.github.com/users/" + username + "/repos" + param + "&per_page=100");
+}
+
+function getTotalStars (repos) {
+    // calculate all the stars that the user has
+    return repos.data.reduce(function (prev, current) {
+        return prev + current.stargazers_count;
+    }, 0);
+}
+
+function getPlayersData (player) {
+    // getRepos
+    // getTotalStars
+    // return object with that data
+    return getRepos(player.login)
+        .then(getTotalStars)
+        .then(function (totalStars) {
+            return {
+                followers: player.followers,
+                totalStars: totalStars
+            };
+        });
+}
+
+function calculateScores (players) {
+    // return an array, after doing some fancy algorithm to determine a winner
+    return [
+        players[0].followers * 3 + players[0].totalStars,
+        players[1].followers * 3 + players[1].totalStars
+    ];
 }
 
 var helpers = {
@@ -19,6 +58,16 @@ var helpers = {
         }).catch(function (err) {
             console.warn("Error in getPlayersInfo", err);
         });
+    },
+    battle: function (players) {
+        var playerOneData = getPlayersData(players[0]);
+        var playerTwoData = getPlayersData(players[1]);
+
+        return axios.all([playerOneData, playerTwoData])
+            .then(calculateScores)
+            .catch(function (err) {
+                console.warn("Error in battle", err);
+            });
     }
 };
 
